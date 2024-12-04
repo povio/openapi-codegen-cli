37d7033e4801b1672e0737e6d1179e2944356c07
Monika Bozhinova <monika.bozhinova@povio.com>
2024-12-04 15:39:27 +0100

    Switch from esm to cjs & Add lib main entry

--- esbuild.mjs
+++ esbuild.mjs
@@ -3,25 +3,21 @@ import fs from "fs";
 
 const packageJson = JSON.parse(fs.readFileSync("package.json", "utf-8"));
 
-await build({
-  entryPoints: ["./src/sh.ts"],
+const buildOptions = {
   bundle: true,
   sourcemap: false,
   platform: "node",
   minify: true,
   metafile: false,
-  format: "esm",
   keepNames: true,
   external: packageJson.dependencies ? Object.keys(packageJson.dependencies) : [],
-  banner: {
-    // hacks to allow commonjs modules to be imported
-    js: "import { createRequire } from 'module'; const require = createRequire(import.meta.url);import * as url from 'url';const __dirname = url.fileURLToPath(new URL('.', import.meta.url));",
-  },
   target: "node14",
   logLevel: "info",
-  outfile: "./dist/sh.js",
   define: {
     "process.env.OPENAPI_CODEGEN_VERSION": `"${packageJson.version}"`,
     "process.env.NODE_ENV": `"production"`,
   },
-});
+};
+
+await build({ ...buildOptions, entryPoints: ["./src/sh.ts"], outfile: "./dist/sh.js" });
+await build({ ...buildOptions, entryPoints: ["./src/index.ts"], outfile: "./dist/index.js" });
