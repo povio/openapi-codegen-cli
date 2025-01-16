4440cb6650aa1f88634eebd9ff5bc1c39422d496
Monika Bozhinova <monika.bozhinova@povio.com>
2025-01-16 18:15:21 +0100

    Add query key all

--- src/generators/utils/generate/generate.query.utils.ts
+++ src/generators/utils/generate/generate.query.utils.ts
@@ -1,4 +1,26 @@
 import { Endpoint } from "../../types/endpoint";
-import { capitalize, snakeToCamel } from "../string.utils";
+import { capitalize, kebabToCamel, snakeToCamel } from "../string.utils";
 
 export const getQueryName = (endpoint: Endpoint) => `use${capitalize(snakeToCamel(endpoint.operationName))}`;
+
+export const getAllQueryKeys = (endpoints: Endpoint[]): { path: string; name: string }[] => {
+  const paths = endpoints.map(({ path }) => path);
+  const firstPathSegmentRegex = /^\/[^\/]*/g;
+  const queryKeys = Array.from(new Set(paths.map((path) => path.match(firstPathSegmentRegex)).flat()));
+  return queryKeys
+    .filter((path) => path !== null)
+    .map((path) => ({
+      path,
+      name: queryKeys.length === 1 ? "all" : `all${capitalize(kebabToCamel(path?.replace(/\//g, "") ?? ""))}`,
+    }));
+};
+
+export const getEndpointPathQueryKeys = (endpoint: Endpoint, endpoints: Endpoint[]) => {
+  const queryKeys = getAllQueryKeys(endpoints);
+  const queryKey = queryKeys.find(({ path }) => endpoint.path.startsWith(path));
+  if (!queryKey) {
+    return [endpoint.path];
+  }
+  const rest = endpoint.path.substring(queryKey.path.length);
+  return [`...keys.${queryKey.name}`, ...(rest ? [`"${rest}"`] : [])];
+};
--- src/generators/utils/hbs/hbs.query.utils.ts
+++ src/generators/utils/hbs/hbs.query.utils.ts
@@ -1,14 +1,26 @@
 import Handlebars from "handlebars";
-import { getQueryName } from "../generate/generate.query.utils";
+import { getAllQueryKeys, getEndpointPathQueryKeys, getQueryName } from "../generate/generate.query.utils";
 
 enum QueryHelpers {
   QUERY_NAME = "queryName",
+  ALL_QUERY_KEYS = "allQueryKeys",
+  ENDPOINT_PATH_QUERY_KEYS = "endpointPathQueryKeys",
 }
 
 export function registerQueryHbsHelpers() {
   registerQueryNameHelper();
+  registerQueryKeysHelper();
+  registerEndpointPathQueryKeysHelper();
 }
 
 function registerQueryNameHelper() {
   Handlebars.registerHelper(QueryHelpers.QUERY_NAME, getQueryName);
+}
+
+function registerQueryKeysHelper() {
+  Handlebars.registerHelper(QueryHelpers.ALL_QUERY_KEYS, getAllQueryKeys);
+}
+
+function registerEndpointPathQueryKeysHelper() {
+  Handlebars.registerHelper(QueryHelpers.ENDPOINT_PATH_QUERY_KEYS, getEndpointPathQueryKeys);
 }
