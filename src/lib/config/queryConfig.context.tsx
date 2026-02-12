ce2546ed73812000e7d17976d7dc93b8e76fe294
Jure Rotar <jrotar0@gmail.com>
2026-02-12 12:23:26 +0100

    feat: added global invalidateCurrentModule options

--- src/lib/config/queryConfig.context.tsx
+++ src/lib/config/queryConfig.context.tsx
@@ -1,16 +1,18 @@
 import { createContext, use, useMemo } from "react";
+import {PropsWithChildren} from "react";
 
 export namespace OpenApiQueryConfig {
   interface Type {
     preferUpdate?: boolean;
+    invalidateCurrentModule?: boolean;
   }
 
   const Context = createContext<Type>({});
 
   type ProviderProps = Type;
 
-  export const Provider = ({ preferUpdate = true, children }: React.PropsWithChildren<ProviderProps>) => {
-    const value = useMemo(() => ({ preferUpdate }), [preferUpdate]);
+  export const Provider = ({ preferUpdate, invalidateCurrentModule, children }: PropsWithChildren<ProviderProps>) => {
+    const value = useMemo(() => ({ preferUpdate, invalidateCurrentModule }), [preferUpdate, invalidateCurrentModule]);
 
     return <Context.Provider value={value}>{children}</Context.Provider>;
   };
