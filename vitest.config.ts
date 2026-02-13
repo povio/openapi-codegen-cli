104393d4877b56eb1fd3bf50c1f862590c0446e7
Izak Glasencnik <izak.glasencnik@povio.com>
2026-02-13 13:01:37 +0100

    fix: tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,17 @@
 /// <reference types="vitest" />
 
 import { defineConfig } from "vitest/config";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
     include: ["src/**/*.test.ts"],
     snapshotFormat: { indent: 4, escapeString: false },
+  },
+  resolve: {
+    alias: {
+      "@": resolve(process.cwd(), "./src"),
+    },
   },
 });
