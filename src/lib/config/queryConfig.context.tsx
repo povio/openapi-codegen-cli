855b53b86e0ec154ad1b89301160e591914d9f53
Jure Rotar <jrotar0@gmail.com>
2026-02-18 12:40:18 +0100

    feat: pass mutation variables to invalidation map and standardize types

--- src/lib/config/queryConfig.context.tsx
+++ src/lib/config/queryConfig.context.tsx
@@ -1,12 +1,18 @@
-import type { QueryKey } from "@tanstack/react-query";
 import { createContext, use, useMemo } from "react";
 import { PropsWithChildren } from "react";
+
+import type { QueryKey } from "@tanstack/react-query";
+
+export type QueryModule = string | number | symbol;
+
+export type InvalidationMapFunc<TData = any, TVariables = any> = (data: TData, variables: TVariables) => QueryKey[];
+export type InvalidationMap<TData = any, TVariables = any> = Record<QueryModule, InvalidationMapFunc<TData, TVariables>>;
 
 export namespace OpenApiQueryConfig {
   interface Type {
     preferUpdate?: boolean;
     invalidateCurrentModule?: boolean;
-    invalidationMap?: Record<string, (context: Record<string, string>) => QueryKey[]>;
+    invalidationMap?: InvalidationMap;
     crossTabInvalidation?: boolean;
   }
 
@@ -14,12 +20,18 @@ export namespace OpenApiQueryConfig {
 
   type ProviderProps = Type;
 
-  export const Provider = ({ preferUpdate, invalidateCurrentModule,
+  export const Provider = ({
+    preferUpdate,
+    invalidateCurrentModule,
     invalidationMap,
     crossTabInvalidation,
 
-    children }: PropsWithChildren<ProviderProps>) => {
-    const value = useMemo(() => ({ preferUpdate, invalidateCurrentModule, invalidationMap, crossTabInvalidation }), [preferUpdate, invalidateCurrentModule, invalidationMap   , crossTabInvalidation]);
+    children,
+  }: PropsWithChildren<ProviderProps>) => {
+    const value = useMemo(
+      () => ({ preferUpdate, invalidateCurrentModule, invalidationMap, crossTabInvalidation }),
+      [preferUpdate, invalidateCurrentModule, invalidationMap, crossTabInvalidation],
+    );
 
     return <Context.Provider value={value}>{children}</Context.Provider>;
   };
