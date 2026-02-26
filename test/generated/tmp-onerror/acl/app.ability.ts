7d1fe80d3771d436f8e270b2c09425e7e1379fe8
Jure Rotar <jrotar0@gmail.com>
2026-02-26 14:55:57 +0100

    chore: removed generated tests

--- test/generated/tmp-onerror/acl/app.ability.ts
+++
@@ -1,5 +0,0 @@
-import { PureAbility, AbilityTuple, Subject } from "@casl/ability";
-
-export type AppAbilities = AbilityTuple<string, Subject>;
-
-export type AppAbility = PureAbility<AppAbilities>;
--- test/generated/tmp-onerror/app-rest-client.ts
+++
@@ -1,7 +0,0 @@
-import { RestClient } from "@povio/openapi-codegen-cli";
-
-export const AppRestClient = new RestClient({
-  config: {
-    baseURL: "https://petstore3.swagger.io/api/v3"
-  },
-});
