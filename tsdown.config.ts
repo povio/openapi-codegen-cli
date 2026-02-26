a8ad5ee122da5ef571633b83c6b2522b59e3f812
Jure Rotar <jrotar0@gmail.com>
2026-02-26 13:35:11 +0100

    chore: converted to pnpm

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "tsdown";
-import { version } from './package.json';
+import packageJson from './package.json' with { type: 'json' };
 
 export default defineConfig({
   entry: ["./src/sh.ts", "./src/generator.ts", "./src/index.ts", "./src/vite.ts", "./src/acl.ts"],
@@ -8,7 +8,7 @@ export default defineConfig({
   target: "esnext",
   dts: true,
   define: {
-    "process.env.OPENAPI_CODEGEN_VERSION": JSON.stringify(version),
+    "process.env.OPENAPI_CODEGEN_VERSION": JSON.stringify(packageJson.version),
     "process.env.NODE_ENV": JSON.stringify("production"),
   },
 });
