e310a7db9da0d204abf2daddf6f56605960cd80d
Urban Lavbič <urban.lavbic@gmail.com>
2026-03-02 09:39:28 +0100

    Fixes

--- src/lib/acl/useAclCheck.ts
+++ src/lib/acl/useAclCheck.ts
@@ -2,7 +2,7 @@ import { useCallback } from "react";
 
 import { type ErrorHandler, SharedErrorHandler } from "../rest/error-handling";
 import { AbilityContext } from "./ability.context";
-import type { AppAbilities } from "./appAbility.types";
+import type { AppAbilities, AppAbility } from "./appAbility.types";
 
 interface UseAclCheckProps {
   errorHandler?: ErrorHandler<never>;
@@ -11,12 +11,12 @@ interface UseAclCheckProps {
 export function useAclCheck<TAppAbilities extends AppAbilities = AppAbilities>({
   errorHandler,
 }: UseAclCheckProps = {}) {
-  const ability = AbilityContext.useAbility<TAppAbilities>();
+  const ability = AbilityContext.useAbility<TAppAbilities>() as AppAbility;
 
   const checkAcl = useCallback(
     (appAbility: TAppAbilities) => {
-      const can = ability.can as unknown as (...appAbility: AppAbilities) => boolean;
-      if (!can(...(appAbility as AppAbilities))) {
+      const [action, subject] = appAbility as AppAbilities;
+      if (!ability.can(action, subject)) {
         (errorHandler ?? SharedErrorHandler).rethrowError(new Error("ACL check failed"));
       }
     },
